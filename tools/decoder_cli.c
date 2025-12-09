4c5a5001fe6561b6e37261d68749603c7010d195
Ronald Caesar <github43132@proton.me>
2025-12-09 16:16:45 -0400

    decoder/tools: Add decoder_cli program
    This program takes a 32-bit hexidecimal representing an ARM instruction
    and prints its corresponding mnemonic. This program will be used in
    tandem with a python fuzzing script to verify the decoder table
    generated by tools/generate_a64_table.py.
    
    Signed-off-by: Ronald Caesar <github43132@proton.me>

---
+++ tools/decoder_cli.c
@@ -0,0 +1,62 @@
+#include "decoder.h"
+#include <errno.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int
+main (int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        fprintf(stderr, "Usage: %s [hex_instruction]\n", argv[0]);
+        return 1;
+    }
+
+    const char *input_string = argv[1];
+    char       *end          = NULL;
+    errno                    = 0;
+
+    int           base  = 16;
+    unsigned long value = strtoul(input_string, &end, base);
+    if (ERANGE == errno)
+    {
+        fprintf(stderr, "Error: input value out of range.\n");
+        return 1;
+    }
+
+    if (input_string == end)
+    {
+        fprintf(stderr, "Error: No hex digits found in input.\n");
+        return 1;
+    }
+
+    if (*end != '\0')
+    {
+        fprintf(
+            stderr, "Error: Trailing garbage characters found: '%s'\n", end);
+        return 1;
+    }
+
+    if (value > UINT32_MAX)
+    {
+        fprintf(stderr,
+                "Error: Value 0x%lX exceeds 32-bit instruction size.\n",
+                value);
+        return 1;
+    }
+
+    uint32_t instruction = (uint32_t)value;
+
+    const bal_decoder_instruction_metadata_t* metadata
+        = bal_decoder_arm64_decode(instruction);
+    if (NULL == metadata)
+    {
+        printf("UNDEFINED\n");
+    }
+    else
+    {
+        printf("%s\n", metadata->name);
+    }
+    return 0;
+}
