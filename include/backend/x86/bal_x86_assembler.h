59e7b4c8a126fb2678fb5c211f2f2b34568241de
Ronald Caesar <github43132@proton.me>
2026-05-22 05:15:51 -0400

    x86/assembler: emit or r64 r64 instructions
    Signed-off-by: Ronald Caesar <github43132@proton.me>

--- include/backend/x86/bal_x86_assembler.h
+++ include/backend/x86/bal_x86_assembler.h
@@ -192,6 +192,21 @@ extern "C"
     /// - `assembler->buffer` is full.
     void bal_x86_emit_ret(bal_x86_assembler_t *assembler);
 
+    /// Emits a bitwise OR instruction between two 64-bit registers.
+    ///
+    /// Assembly equivalent: `or destination, source`.
+    ///
+    /// # Safety
+    ///
+    /// This function fails if:
+    ///
+    /// - `assembler` is `NULL`.
+    /// - `assembler->status != [`BAL_SUCCESS`]
+    /// - `assembler->buffer` is full.
+    void bal_x86_emit_or_r64_r64(bal_x86_assembler_t *assembler,
+                                 bal_x86_register_t   destination,
+                                 bal_x86_register_t   source);
+
     /// Emits a Stack Push instruction.
     ///
     /// Assembly equivalent: `push reg64`
